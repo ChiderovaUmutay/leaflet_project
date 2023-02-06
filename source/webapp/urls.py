ac248fe962d37a52f1f9ab61bbbc52135aafb1dc
umutay <chiderova08@gmail.com>
2023-02-06 18:22:35 +0600

    #8 Создала представление "AreaAdd", где реализовала добавление района.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views import PlotAdd
+from webapp.views import PlotAdd, AreaAdd
 
 urlpatterns = [
-    path('area/<int:pk>/plot/add/', PlotAdd.as_view()),
+    path("area/<int:pk>/plot/add/", PlotAdd.as_view()),
+    path("region/<int:pk>/area/add/", AreaAdd.as_view())
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -3,23 +3,39 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from webapp.models import Area, Plot
-from webapp.serializer import PlotSerializer
+from webapp.models import Area, Plot, Region
+from webapp.serializer import PlotSerializer, AreaSerializer
+
+serializer_data = {}
+
+
+def get_serializer_data(obj, related_object, related_object_field=None):
+    serializer_data["id"] = obj.id
+    serializer_data["name"] = obj.name
+    serializer_data["coordinates"] = obj.coordinates
+    if related_object_field:
+        serializer_data[related_object_field] = related_object
+    return serializer_data
 
 
 class PlotAdd(APIView):
-    serializer_class = PlotSerializer
-    serializer_data = {}
-
-    def get_serializer_data(self, plot, area):
-        self.serializer_data["id"] = plot.id
-        self.serializer_data["name"] = plot.name
-        self.serializer_data["coordinates"] = plot.coordinates
-        self.serializer_data["area"] = area
+    plot_serializer_class = PlotSerializer
 
     def post(self, request, *args, **kwargs):
         area = get_object_or_404(Area, pk=kwargs.get("pk"))
         new_plot = Plot.objects.create(**request.data, area=area)
-        self.get_serializer_data(new_plot, area)
-        serializer = self.serializer_class(self.serializer_data).data
+        plot_serializer_data = get_serializer_data(new_plot, area, related_object_field="area")
+        serializer = self.plot_serializer_class(plot_serializer_data).data
         return Response(serializer)
+
+
+class AreaAdd(APIView):
+    area_serializer_class = AreaSerializer
+
+    def post(self, request, *args, **kwargs):
+        region = get_object_or_404(Region, pk=kwargs.get("pk"))
+        new_area = Area.objects.create(**request.data, region=region)
+        area_serializer_data = get_serializer_data(new_area, region, related_object_field="region")
+        serializer = self.area_serializer_class(area_serializer_data).data
+        return Response(serializer)
+
