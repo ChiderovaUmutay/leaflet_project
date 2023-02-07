eb4f9d97452b4dc4d1ae3c02f66e2cb750f11760
umutay <chiderova08@gmail.com>
2023-02-07 15:54:51 +0600

    #19 Переместила директории "hello", "webapp" и файл "manage.py" на одну директорию назад

--- hello/settings.py
+++ hello/settings.py
@@ -90,25 +90,27 @@ DATABASES = {
 }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
 
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- webapp/urls.py
+++ webapp/urls.py
@@ -3,11 +3,13 @@ from rest_framework.routers import DefaultRouter
 
 from webapp.views import PlotView, AreaView, RegionViewSet
 
+app_name = "webapp"
+
 router = DefaultRouter()
 router.register("regions", RegionViewSet)
 
 urlpatterns = [
-    path("area/<int:pk>/plot/add/", PlotView.as_view()),
+    path("area/<int:pk>/plot/", PlotView.as_view()),
     path("plot/<int:pk>/", PlotView.as_view()),
     path("region/<int:pk>/area/add/", AreaView.as_view()),
     path("area/<int:pk>/", AreaView.as_view()),
